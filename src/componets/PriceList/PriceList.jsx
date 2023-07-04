0e6b1024be77ec177932aedf9913bd633dc6ae76
Saidul-06 <saidul15-2703@diu.edu.bd>
2023-07-04 23:10:56 +0600

    first commit

---
+++ src/componets/PriceList/PriceList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PriceList = () => {
+    return (
+        <div className='text-7xl bg-purple-700 font-bold p-4 text-center  text-purple-300'>
+            <h1>Awsome price</h1>
+        </div>
+    );
+};
+
+export default PriceList;
