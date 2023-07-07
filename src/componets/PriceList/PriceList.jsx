3362c955f11e11e49e301c2f9a00a525525564b8
Saidul-06 <saidul15-2703@diu.edu.bd>
2023-07-07 22:35:07 +0600

    Starting

--- src/componets/PriceList/PriceList.jsx
+++ src/componets/PriceList/PriceList.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const PriceList = () => {
+    const [prices,setPrices]= useState([]);
     return (
         <div className='text-7xl bg-purple-700 font-bold p-4 text-center  text-purple-300'>
             <h1>Awsome price</h1>
